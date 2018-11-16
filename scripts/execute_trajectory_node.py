0dba1140199c7790b89d59961af3a4f91aabd1f3
Travers Rhodes <traversr@andrew.cmu.edu>
2018-11-15 22:19:07 -0500

    compiles and runs, but either the generated trajectory is (at best) relative to a rotated frame

---
+++ scripts/execute_trajectory_node.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+import time
+
+import feedbot_trajectory_logic.tracker_interface as tracker
+import numpy as np
+from learn_trajectory.srv import PlayTrajectory
+from std_msgs.msg import String, Empty
+from geometry_msgs.msg import Quaternion
+
+
+class SpoonFeeder:
+  def __init__(self):
+    rospy.logwarn("sleeping for 5 seconds before starting feeding")
+    rospy.sleep(5)
+    # quaternion is defined in order x,y,z,w
+    self.defaultQuat = Quaternion(0.5, 0.5, 0.5, 0.5)
+    self.tracker = tracker.TrackerInterface(self.defaultQuat)
+    self.play_trajectory_topic = "/trained_poses"
+    self._play_trajectory = rospy.ServiceProxy("play_trajectory", PlayTrajectory)
+
+  def follow_trajectory(self):
+    self.tracker.start_updating_target_to_pose(self.play_trajectory_topic)
+    self._play_trajectory(String(self.play_trajectory_topic))
+
+if __name__=="__main__":
+  rospy.init_node('spoon_feeder', anonymous=True)
+  s = SpoonFeeder()
+  while (True):
+    s.follow_trajectory()
+    time.sleep(5)
